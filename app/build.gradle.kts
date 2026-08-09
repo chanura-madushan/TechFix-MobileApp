e6b2eb77082def6e0da9e6e8d81bf733b815de5d
chanura-madushan <chanuramadushan18@gmail.com>
2026-08-09 15:23:23 +0530

    started working on location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
     implementation(libs.androidx.recyclerview)
+    implementation(libs.play.services.location)
 }
