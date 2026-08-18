e79ce98f09ef7d4b77a03e4556b111d43d546fed
AlphaOshyy <oshantanusha38@gmail.com>
2026-08-18 11:36:31 +0530

    Convert MainActivity to Java

---
+++ app/src/main/java/com/example/techfix/MainActivity.java
@@ -0,0 +1,3 @@
+package com.example.techfix;
+import android.os.Bundle; import androidx.appcompat.app.AppCompatActivity; import androidx.activity.EdgeToEdge; import androidx.core.graphics.Insets; import androidx.core.view.ViewCompat; import androidx.core.view.WindowInsetsCompat;
+public class MainActivity extends AppCompatActivity { @Override protected void onCreate(Bundle savedInstanceState){super.onCreate(savedInstanceState); EdgeToEdge.enable(this); setContentView(R.layout.activity_main); ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main),(v,insets)->{Insets i=insets.getInsets(WindowInsetsCompat.Type.systemBars());v.setPadding(i.left,i.top,i.right,i.bottom);return insets;});} }
