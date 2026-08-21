a5ae9df2cb5b1469802266c64ec111d807ee7bb8
chanura-madushan <chanuramadushan18@gmail.com>
2026-08-21 13:09:59 +0530

    device category class created

---
+++ app/src/main/java/com/example/techfix/model/DeviceCategory.java
@@ -0,0 +1,12 @@
+package com.example.techfix.model;
+
+public class DeviceCategory {
+    public String categoryId;
+    public String name;
+
+    public DeviceCategory() {}
+
+    public DeviceCategory(String name) {
+        this.name = name;
+    }
+}
